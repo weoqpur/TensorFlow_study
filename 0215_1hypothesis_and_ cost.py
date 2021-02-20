d60714ab8389cb2b0e7fdcccff56986459ca462c
weoqpur <tlsanruf123@gmail.com>
2021-02-21 05:24:50 +0900

    선형회귀1

---
+++ 0215_1hypothesis_and_ cost.py
@@ -0,0 +1,32 @@
+import tensorflow as tf
+import numpy as np
+
+# 인풋 데이터
+x_data = [1, 2, 3, 4, 5] 
+# 아웃풋 데이터
+y_data = [1, 2, 3, 4, 5]
+
+# 원래는 랜덤 값을 지정함(임의로 값을 넣어 봄)
+w = tf.Variable(2.9)
+b = tf.Variable(0.5)
+
+# print(cost)
+
+# learning_rate initialize
+learning_rate = 0.01
+
+# gradient descent
+for i in range(100+1):
+    with tf.GradientTape() as tape:
+        # H(x) = Wx + b
+        hypothesis = w * x_data + b
+        # 랭크를 줄이고 평균을 구하는 함수 reduce_mean
+        # 제곱을 해주는 함수 square
+        cost = tf.reduce_mean(tf.square(hypothesis - y_data))
+
+    w_grad, b_grad = tape.gradient(cost, [w, b])
+
+    w.assign_sub(learning_rate * w_grad)
+    b.assign_sub(learning_rate * b_grad)
+    if i % 10 == 0:
+        print("{:5}|{:10.4f}|{:10.4}|{:10.6f}".format(i, w.numpy(), b.numpy(), cost))
