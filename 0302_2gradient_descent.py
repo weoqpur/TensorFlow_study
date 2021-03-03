395b9487f7f7358d37507d07b4d9d4f7db3cd9c0
weoqpur <tlsanruf123@gmail.com>
2021-03-04 08:39:42 +0900

    경사하강법1

--- 0302_2gradient_descent.py
+++ 0302_2gradient_descent.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import numpy as np
 
+# 랜덤 시드 초기화 다음에도 실행 했을때 똑같은 값이 나오게
 tf.random.set_seed(0)
 
 x_data = [1., 2., 3., 4.]
@@ -14,6 +15,7 @@ for step in range(300):
     cost = tf.reduce_mean(tf.square(hypothesis - y_data))
 
     alpha = 0.01
+    # multiply : 두 수를 곱해 줌
     gradient = tf.reduce_mean(tf.multiply(tf.multiply(W, x_data) - y_data, x_data))
     descent = W - tf.multiply(alpha, gradient)
     W.assign(descent)
