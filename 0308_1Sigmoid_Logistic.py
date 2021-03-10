a1a36236adb2024e81dfb79ac74403d87e7d64aa
weoqpur <tlsanruf123@gmail.com>
2021-03-11 08:03:30 +0900

    3가지의 함수로 데이터를 연산 할 수 있게 만들었습니다.

--- 0308_1Sigmoid_Logistic.py
+++ 0308_1Sigmoid_Logistic.py
@@ -24,16 +24,22 @@ plt.xlabel("x1")
 plt.xlabel("x2")
 plt.show()
 
+# 1행 2열의 데이터셋을 6개 생성
 dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).batch(len(x_train))#.repeat()
 
+# 0으로 채워진 2행 1열의 데이터를 생성
 W = tf.Variable(tf.zeros([2,1]), name = 'weight')
+# 0으로 채워진 1행 1열의 데이터를 생성
 B = tf.Variable(tf.zeros([1]), name='bias')
 
 def logistic_regression(features):
+    # 1 + e^-x / 1
+    # tf.divide() 두 데이터를 나눠 줌 
     hypothesis = tf.divide(1., 1. + tf.exp(tf.matmul(features, W) + B))
     return hypothesis
 
 def loss_fn(hypothesis, labels):
+    # cost(h(x),y) = -labelLog(h(x)) - (1 - label)log(1 - h(x))
     cost = -tf.reduce_mean(labels * tf.math.log(hypothesis) + (1 - labels) * tf.math.log(1 - hypothesis))
     return cost
 
